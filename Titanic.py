3e8aa05bef878c357d96e3974e0bc9c83b9dc089
Ivan <ivan.shelonik@gmail.com>
2017-03-26 20:37:44 +0300

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -3,36 +3,6 @@ from sklearn import cross_validation, neighbors
 from sklearn import tree
 import pandas as pd
 
-'''
-Ivan Shelonik
-
-The task https://www.kaggle.com/c/titanic
-
-The training set should be used to build your machine learning models.
-For the training set, we provide the outcome (also known as the “ground truth”)
-for each passenger. Your model will be based on “features” like passengers’ gender
-and class. You can also use feature engineering to create new features.
-
-The test set should be used to see how well your model performs on unseen data.
-For the test set, we do not provide the ground truth for each passenger.
-It is your job to predict these outcomes. For each passenger in the test set,
-use the model you trained to predict whether or not they survived the sinking of the Titanic.
-
-Data Dictionary
-
-Variable	Definition	Key
-survival	Survival	0 = No, 1 = Yes
-pclass	Ticket class	1 = 1st, 2 = 2nd, 3 = 3rd
-sex	Sex
-Age	Age in years
-sibsp	# of siblings / spouses aboard the Titanic
-parch	# of parents / children aboard the Titanic
-ticket	Ticket number
-fare	Passenger fare
-cabin	Cabin number
-embarked	Port of Embarkation	C = Cherbourg, Q = Queenstown, S = Southampton
-'''
-
 
 #Loading data
 df_train = pd.read_csv('train.csv')
